917b3df53eb764b84a658986b511de9979a46697
justinsteinberg <justin.steinberg1@aol.com>
2023-12-09 18:44:19 -0500

    Styling writing a review page.

--- src/Posts/index.js
+++ src/Posts/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AddPostForm from 'src/Posts/addPostForm';
 import {useParams} from 'react-router-dom';
-
+import './styling/posts.css'
 const ReviewsPage = () => {
 
     const {rId} = useParams();
 
     return (
-        <div>
+        <div className="addReviews">
             <h1>Add Your Review</h1>
             <AddPostForm restaurantId={rId}/>
         </div>
