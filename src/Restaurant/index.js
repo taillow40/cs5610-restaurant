33c897ad4ccfc4cc5be13aeed13ea7a84b284ed9
Kyle Koo <kylekoo837@gmail.com>
2023-12-10 12:39:28 -0500

    made restaurant page more responsive, deleted old temp database

--- src/Restaurant/index.js
+++ src/Restaurant/index.js
@@ -8,37 +8,50 @@ import { useParams } from "react-router-dom";
 import HeaderBox from "./HeaderBox";
 
 function Restaurant() {
-    const { rId } = useParams();
+  const { rId } = useParams();
 
-    const [restaurant, setRestaurant] = useState({});
-    const [reviews, setReviews] = useState([]);
+  const [restaurant, setRestaurant] = useState({});
+  const [reviews, setReviews] = useState([]);
 
-    useEffect(() => {
-      const findRestaurant = async () => {
-        const restaurant = await restaurantClient.findRestaurantById(rId);
-        setRestaurant(restaurant);
-      };
-      const findAllReviews = async () => {
-        const reviews = await restaurantClient.reviews(rId);
-        setReviews(reviews);
-      };
-      findRestaurant();
-      findAllReviews();
-    }, []);
+  useEffect(() => {
+    const findRestaurant = async () => {
+      const restaurant = await restaurantClient.findRestaurantById(rId);
+      setRestaurant(restaurant);
+    };
+    const findAllReviews = async () => {
+      const reviews = await restaurantClient.reviews(rId);
+      setReviews(reviews);
+    };
+    findRestaurant();
+    findAllReviews();
+  }, []);
 
-    return (
-        <div className="flex-down">
-            <div className="restaurant-card">
-            {restaurant && <HeaderBox restaurant={restaurant} reviews={reviews}/>}
-            </div>
-            <div className="restaurant-card">
-            {restaurant &&<SummaryBox restaurant={restaurant} reviews={reviews}/>}
-            </div>
-            <div className="restaurant-card">
-            {restaurant &&<ReviewsBox restaurant={restaurant} reviews={reviews}/>}
-            </div>
+  return (
+    <div className="flex-down">
+      <div class="d-none d-md-block">
+        <div className="restaurant-card">
+          {restaurant && <HeaderBox restaurant={restaurant} reviews={reviews} />}
         </div>
-    )
+        <div className="restaurant-card">
+          {restaurant && <SummaryBox restaurant={restaurant} reviews={reviews} />}
+        </div>
+        <div className="restaurant-card">
+          {restaurant && <ReviewsBox restaurant={restaurant} reviews={reviews} />}
+        </div>
+      </div>
+      <div class="d-block d-md-none">
+        <div className="restaurant-card-mobile">
+          {restaurant && <HeaderBox restaurant={restaurant} reviews={reviews} />}
+        </div>
+        <div className="restaurant-card-mobile">
+          {restaurant && <SummaryBox restaurant={restaurant} reviews={reviews} />}
+        </div>
+        <div className="restaurant-card-mobile">
+          {restaurant && <ReviewsBox restaurant={restaurant} reviews={reviews} />}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Restaurant;
--- src/Restaurant/temp-database/index.js
+++
@@ -1,7 +0,0 @@
-import restaurants from "./restaurants.json";
-import reviews from "./reviews.json";
-
-export default {
-    restaurants,
-    reviews
-}
